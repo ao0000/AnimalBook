85496a536efba8f6eee19a3ef6aa994ba51f1f05
ao0000 <e1q19041@oit.ac.jp>
2021-02-13 17:16:56 +0900

    add groupie

--- app/src/main/java/me/aofz/acfb/ui/GalleryFragment.kt
+++ app/src/main/java/me/aofz/acfb/ui/GalleryFragment.kt
@@ -7,14 +7,18 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import com.xwray.groupie.Group
+import com.xwray.groupie.GroupieAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import me.aofz.acfb.databinding.GalleryFragmentBinding
+import me.aofz.acfb.model.Fish
 
 @AndroidEntryPoint
 class GalleryFragment : Fragment() {
 
     private lateinit var binding: GalleryFragmentBinding
     private val viewModel by viewModels<GalleryViewModel>()
+    private val adapter = GroupieAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,10 +31,20 @@ class GalleryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.galleryContainer.adapter = adapter
+        gallaryObserver()
         viewModel.fetchRemote()
+    }
+
+    private fun gallaryObserver() {
         viewModel.collection.observe(viewLifecycleOwner, Observer {
-            it?.let {
-                binding.sampleText.text = it.get(0).iconUri
+            it?.let { fishList ->
+                adapter.update(mutableListOf<Group>().apply {
+                    fishList.forEach { fish ->
+                        this.add(GalleryItem(fish))
+                    }
+                })
             }
         })
     }
--- app/src/main/java/me/aofz/acfb/ui/GalleryItem.kt
+++ app/src/main/java/me/aofz/acfb/ui/GalleryItem.kt
@@ -1,4 +1,20 @@
 package me.aofz.acfb.ui
 
-class GalleryItem {
+import android.view.View
+import com.xwray.groupie.viewbinding.BindableItem
+import me.aofz.acfb.R
+import me.aofz.acfb.databinding.GalleryItemBinding
+import me.aofz.acfb.model.Fish
+
+class GalleryItem(val fish: Fish) : BindableItem<GalleryItemBinding>() {
+
+    override fun getLayout(): Int = R.layout.gallery_item
+
+    override fun bind(viewBinding: GalleryItemBinding, position: Int) {
+        viewBinding.nameText.text = fish.name
+        viewBinding.displayPriceText.text = fish.price.toString()
+    }
+
+    override fun initializeViewBinding(view: View): GalleryItemBinding =
+        GalleryItemBinding.bind(view)
 }
