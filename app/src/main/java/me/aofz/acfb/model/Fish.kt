e15d49667f363d4bac506ae6954e0b733b30a47f
ao0000 <e1q19041@oit.ac.jp>
2021-02-16 01:12:43 +0900

    make model parcelable

--- app/src/main/java/me/aofz/acfb/model/Fish.kt
+++ app/src/main/java/me/aofz/acfb/model/Fish.kt
@@ -1,9 +1,13 @@
 package me.aofz.acfb.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Fish(
     val id: Int,
     val name: String,
     val price: Int,
     val iconUri: String,
     val imageUri: String
-)
+) : Parcelable
