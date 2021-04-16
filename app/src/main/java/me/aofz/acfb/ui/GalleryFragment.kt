af6a99909fc96df6526468139babe9b3977b19b5
ao0000 <e1q19041@oit.ac.jp>
2021-04-16 16:30:58 +0900

    measure for memory leak in view binding

--- app/src/main/java/me/aofz/acfb/ui/GalleryFragment.kt
+++ app/src/main/java/me/aofz/acfb/ui/GalleryFragment.kt
@@ -1,36 +1,27 @@
 package me.aofz.acfb.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import by.kirich1409.viewbindingdelegate.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
+import me.aofz.acfb.R
 import me.aofz.acfb.databinding.GalleryFragmentBinding
 import me.aofz.acfb.model.ResourceState
 import me.aofz.acfb.ui.adapter.GalleryAdapter
 
 @AndroidEntryPoint
-class GalleryFragment : Fragment() {
+class GalleryFragment : Fragment(R.layout.gallery_fragment) {
 
-    private lateinit var binding: GalleryFragmentBinding
+    private val binding: GalleryFragmentBinding by viewBinding()
 
     private val viewModel: GalleryViewModel by viewModels()
 
     private val galleryAdapter = GalleryAdapter()
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = GalleryFragmentBinding.inflate(inflater, container, false)
-        return binding.root
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
