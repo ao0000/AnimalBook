e1f974a649e7e74e3514a4e4c399917265061e7c
ao0000 <e1q19041@oit.ac.jp>
2021-06-22 14:50:11 +0900

    add detail view

--- app/src/main/java/me/aofz/acfb/ui/detail/DetailFragment.kt
+++ app/src/main/java/me/aofz/acfb/ui/detail/DetailFragment.kt
@@ -2,15 +2,26 @@ package me.aofz.acfb.ui.detail
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
 import by.kirich1409.viewbindingdelegate.viewBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import me.aofz.acfb.R
 import me.aofz.acfb.databinding.DetailFragmentBinding
+import me.aofz.acfb.ext.setImage
+import me.aofz.acfb.model.LoadingState
 
+@AndroidEntryPoint
 class DetailFragment : Fragment(R.layout.detail_fragment) {
 
     private val binding: DetailFragmentBinding by viewBinding()
+
+    private val viewModel: DetailViewModel by viewModels()
 
     private val args: DetailFragmentArgs by navArgs()
 
@@ -18,6 +29,34 @@ class DetailFragment : Fragment(R.layout.detail_fragment) {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
             lifecycleOwner = viewLifecycleOwner
+        }
+
+        viewModel.fetchItem(args.animalType, args.id)
+        lifecycleScope.launch {
+            viewModel.uiState.collect { viewState ->
+                when (viewState) {
+                    is LoadingState.Loading -> {
+                        val loadingText: String = resources.getString(R.string.loading_toast_text)
+                        Toast.makeText(context, loadingText, Toast.LENGTH_SHORT).show()
+                    }
+                    is LoadingState.Success -> {
+                        viewState.data.apply {
+                            binding.let {
+                                it.itemName.text = name
+                                it.itemPrice.text = price.toString()
+                                it.detailIconImage.setImage(iconUri)
+                                it.detailImage.setImage(imageUri)
+                            }
+                        }
+                        val successText: String = resources.getString(R.string.success_toast_text)
+                        Toast.makeText(context, successText, Toast.LENGTH_SHORT).show()
+                    }
+                    is LoadingState.Error -> {
+                        val errorText: String = resources.getString(R.string.error_toast_text)
+                        Toast.makeText(context, errorText, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/me/aofz/acfb/ui/detail/DetailViewModel.kt
+++ app/src/main/java/me/aofz/acfb/ui/detail/DetailViewModel.kt
@@ -1,4 +1,46 @@
 package me.aofz.acfb.ui.detail
 
-class DetailViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import me.aofz.acfb.model.Animal
+import me.aofz.acfb.model.AnimalType
+import me.aofz.acfb.model.LoadingState
+import me.aofz.acfb.repository.Repository
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailViewModel @Inject constructor(private val repository: Repository) : ViewModel() {
+
+    private val _uiState: MutableStateFlow<LoadingState<Animal>> =
+        MutableStateFlow(LoadingState.Loading)
+
+    val uiState: StateFlow<LoadingState<Animal>>
+        get() = _uiState
+
+    fun fetchItem(animalType: AnimalType, animalId: Int) {
+        viewModelScope.launch {
+            when (animalType) {
+                AnimalType.FISH -> {
+                    repository.getFish(animalId).collect { viewState ->
+                        _uiState.value = viewState
+                    }
+                }
+                AnimalType.BUG -> {
+                    repository.getBug(animalId).collect { viewState ->
+                        _uiState.value = viewState
+                    }
+                }
+                AnimalType.SEA_CREATURE -> {
+                    repository.getSeaCreature(animalId).collect { viewState ->
+                        _uiState.value = viewState
+                    }
+                }
+            }
+        }
+    }
 }
