4c2c06e73e5db805c84927d72b59a2ee24d6e45f
ao0000 <e1q19041@oit.ac.jp>
2021-06-24 16:53:18 +0900

    change to make new coroutine according to lifecycle

--- app/src/main/java/me/aofz/acfb/ui/collection/gallery/GalleryFragment.kt
+++ app/src/main/java/me/aofz/acfb/ui/collection/gallery/GalleryFragment.kt
@@ -5,10 +5,13 @@ import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import by.kirich1409.viewbindingdelegate.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import me.aofz.acfb.R
 import me.aofz.acfb.databinding.GalleryFragmentBinding
 import me.aofz.acfb.model.AnimalType
@@ -32,28 +35,30 @@ class GalleryFragment(onClickItem: (animalType: AnimalType, id: Int) -> Unit) :
             lifecycleOwner = viewLifecycleOwner
             galleryContainer.adapter = adapter
         }
-
-        fetchItem()
-
-        lifecycleScope.launchWhenStarted {
-            viewModel.uiState.collect { uiState ->
-                when (uiState) {
-                    is LoadingState.Loading -> {
-                        val loadingText: String = resources.getString(R.string.loading_toast_text)
-                        Toast.makeText(context, loadingText, Toast.LENGTH_SHORT).show()
-                    }
-                    is LoadingState.Success -> {
-                        adapter.submitList(uiState.data)
-                        val successText: String = resources.getString(R.string.success_toast_text)
-                        Toast.makeText(context, successText, Toast.LENGTH_SHORT).show()
-                    }
-                    is LoadingState.Error -> {
-                        val errorText: String = resources.getString(R.string.error_toast_text)
-                        Toast.makeText(context, errorText, Toast.LENGTH_SHORT).show()
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect { uiState ->
+                    when (uiState) {
+                        is LoadingState.Loading -> {
+                            val loadingText: String =
+                                resources.getString(R.string.loading_toast_text)
+                            Toast.makeText(context, loadingText, Toast.LENGTH_SHORT).show()
+                        }
+                        is LoadingState.Success -> {
+                            adapter.submitList(uiState.data)
+                            val successText: String =
+                                resources.getString(R.string.success_toast_text)
+                            Toast.makeText(context, successText, Toast.LENGTH_SHORT).show()
+                        }
+                        is LoadingState.Error -> {
+                            val errorText: String = resources.getString(R.string.error_toast_text)
+                            Toast.makeText(context, errorText, Toast.LENGTH_SHORT).show()
+                        }
                     }
                 }
             }
         }
+        fetchItem()
     }
 
     private fun fetchItem() {
--- app/src/main/java/me/aofz/acfb/ui/detail/DetailFragment.kt
+++ app/src/main/java/me/aofz/acfb/ui/detail/DetailFragment.kt
@@ -5,7 +5,9 @@ import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.navArgs
 import by.kirich1409.viewbindingdelegate.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -30,25 +32,29 @@ class DetailFragment : Fragment(R.layout.detail_fragment) {
             lifecycleOwner = viewLifecycleOwner
         }
 
-        viewModel.fetchItem(args.animalType, args.id)
         lifecycleScope.launch {
-            viewModel.uiState.collect { viewState ->
-                when (viewState) {
-                    is LoadingState.Loading -> {
-                        val loadingText: String = resources.getString(R.string.loading_toast_text)
-                        Toast.makeText(context, loadingText, Toast.LENGTH_SHORT).show()
-                    }
-                    is LoadingState.Success -> {
-                        binding.animal = viewState.data
-                        val successText: String = resources.getString(R.string.success_toast_text)
-                        Toast.makeText(context, successText, Toast.LENGTH_SHORT).show()
-                    }
-                    is LoadingState.Error -> {
-                        val errorText: String = resources.getString(R.string.error_toast_text)
-                        Toast.makeText(context, errorText, Toast.LENGTH_SHORT).show()
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect { viewState ->
+                    when (viewState) {
+                        is LoadingState.Loading -> {
+                            val loadingText: String =
+                                resources.getString(R.string.loading_toast_text)
+                            Toast.makeText(context, loadingText, Toast.LENGTH_SHORT).show()
+                        }
+                        is LoadingState.Success -> {
+                            binding.animal = viewState.data
+                            val successText: String =
+                                resources.getString(R.string.success_toast_text)
+                            Toast.makeText(context, successText, Toast.LENGTH_SHORT).show()
+                        }
+                        is LoadingState.Error -> {
+                            val errorText: String = resources.getString(R.string.error_toast_text)
+                            Toast.makeText(context, errorText, Toast.LENGTH_SHORT).show()
+                        }
                     }
                 }
             }
         }
+        viewModel.fetchItem(args.animalType, args.id)
     }
 }
